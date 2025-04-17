bf224af5d9a8e55bb2d79c61a4e1d15e49acb3b7
timtim123125 <73978389+timtim123125@users.noreply.github.com>
2025-04-17 19:07:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -165,7 +165,7 @@ if input_type == "Phishing Email":
                     st.pyplot(st.session_state.chart)
 
             st.session_state.form_submitted = False
-            st.rerun()
+            #st.rerun()
 
 else:
     # Handle News Article input (using form and rerun)
